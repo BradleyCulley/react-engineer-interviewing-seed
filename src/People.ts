604e5c2582f7c1973d1747d37220a48e4b24089c
Brad Culley <culleyib@gmail.com>
2023-01-23 09:18:11 -0500

    Initial commit

---
+++ src/People.ts
@@ -0,0 +1,242 @@
+export const PEOPLE = [
+    {
+        "guid": "429629dc-b4a5-40bf-afc5-13dfb7f32f1b",
+        "isActive": false,
+        "name": "Ross Sharpe",
+        "age": 32,
+        "email": "rosssharpe@slambda.com",
+        "phone": "+1 (915) 417-3078",
+        "address": "751 Green Street, Sidman, West Virginia, 7298",
+        "registered": "2016-07-01T11:31:54 +04:00"
+    },
+    {
+        "guid": "e38cfd3c-508e-4d69-98c0-9a848f53276d",
+        "isActive": true,
+        "name": "Washington Day",
+        "age": 27,
+        "email": "washingtonday@slambda.com",
+        "phone": "+1 (845) 503-2683",
+        "address": "932 Richardson Street, Harold, Vermont, 796",
+        "registered": "2019-02-17T05:16:15 +05:00"
+    },
+    {
+        "guid": "0f033caa-0e79-409b-843d-cda4ab20edc0",
+        "isActive": false,
+        "name": "Mcdaniel Mccall",
+        "age": 23,
+        "email": "mcdanielmccall@slambda.com",
+        "phone": "+1 (938) 516-3179",
+        "address": "942 Graham Avenue, Shasta, Indiana, 2551",
+        "registered": "2014-09-07T12:16:38 +04:00"
+    },
+    {
+        "guid": "06db9868-1fdb-47ac-b76d-51c4500855c5",
+        "isActive": false,
+        "name": "Mejia Gill",
+        "age": 30,
+        "email": "mejiagill@slambda.com",
+        "phone": "+1 (842) 525-2791",
+        "address": "347 Franklin Street, Lindisfarne, Maryland, 3728",
+        "registered": "2018-06-03T06:27:44 +04:00"
+    },
+    {
+        "guid": "487d1853-5e51-4b82-bdb8-bb4cd39d2d5f",
+        "isActive": true,
+        "name": "Todd Grant",
+        "age": 35,
+        "email": "toddgrant@slambda.com",
+        "phone": "+1 (975) 475-2323",
+        "address": "509 Euclid Avenue, Belfair, Utah, 5641",
+        "registered": "2019-05-12T09:00:38 +04:00"
+    },
+    {
+        "guid": "4d8de8bb-1eb3-47cb-a9c0-3a27f2cece81",
+        "isActive": false,
+        "name": "Contreras Martin",
+        "age": 39,
+        "email": "contrerasmartin@slambda.com",
+        "phone": "+1 (860) 455-2337",
+        "address": "408 Ainslie Street, Crumpler, Georgia, 314",
+        "registered": "2014-12-15T10:41:29 +05:00"
+    },
+    {
+        "guid": "920416a0-fd57-4896-9ea9-9a037ffdb24b",
+        "isActive": false,
+        "name": "Kerr Dotson",
+        "age": 31,
+        "email": "kerrdotson@slambda.com",
+        "phone": "+1 (804) 595-3820",
+        "address": "102 High Street, Ripley, Virgin Islands, 9735",
+        "registered": "2016-07-30T09:08:48 +04:00"
+    },
+    {
+        "guid": "00b7f52d-6a36-457a-8a0f-534b79c63aff",
+        "isActive": true,
+        "name": "Newton Church",
+        "age": 25,
+        "email": "newtonchurch@slambda.com",
+        "phone": "+1 (976) 548-2683",
+        "address": "513 Foster Avenue, Frystown, Iowa, 9122",
+        "registered": "2018-04-29T03:42:43 +04:00"
+    },
+    {
+        "guid": "52bbf9e6-cb83-402a-b3e5-e0c7e1ea6fbc",
+        "isActive": false,
+        "name": "Mcbride Hammond",
+        "age": 34,
+        "email": "mcbridehammond@slambda.com",
+        "phone": "+1 (838) 402-3956",
+        "address": "212 Paerdegat Avenue, Mayfair, Delaware, 495",
+        "registered": "2014-03-27T03:51:50 +04:00"
+    },
+    {
+        "guid": "cacd4f4a-afd4-47c3-b783-590bd8bd5913",
+        "isActive": true,
+        "name": "Cunningham Moon",
+        "age": 35,
+        "email": "cunninghammoon@slambda.com",
+        "phone": "+1 (864) 493-2227",
+        "address": "301 Verona Street, Genoa, Nebraska, 1408",
+        "registered": "2018-12-03T03:47:28 +05:00"
+    },
+    {
+        "guid": "af7c0424-1d6d-49a8-a038-6478bfbd6f65",
+        "isActive": false,
+        "name": "Gordon Torres",
+        "age": 21,
+        "email": "gordontorres@slambda.com",
+        "phone": "+1 (817) 541-3136",
+        "address": "690 Logan Street, Abrams, Oregon, 6852",
+        "registered": "2019-12-21T02:29:20 +05:00"
+    },
+    {
+        "guid": "bd597bf9-abf8-4d32-8829-6d3510dd5313",
+        "isActive": false,
+        "name": "Gamble Vargas",
+        "age": 33,
+        "email": "gamblevargas@slambda.com",
+        "phone": "+1 (997) 535-2272",
+        "address": "339 Maujer Street, Verdi, Michigan, 342",
+        "registered": "2017-09-21T03:46:50 +04:00"
+    },
+    {
+        "guid": "b2a6205e-a5fa-483b-bb35-5a5ff09c6c79",
+        "isActive": false,
+        "name": "Melba Maxwell",
+        "age": 37,
+        "email": "melbamaxwell@slambda.com",
+        "phone": "+1 (890) 594-3842",
+        "address": "184 Cheever Place, Harmon, Texas, 5460",
+        "registered": "2019-02-09T12:45:00 +05:00"
+    },
+    {
+        "guid": "45debd53-0407-401c-919f-f5c15adf1ddd",
+        "isActive": false,
+        "name": "Tessa Avila",
+        "age": 35,
+        "email": "tessaavila@slambda.com",
+        "phone": "+1 (880) 579-3947",
+        "address": "364 Story Street, Camptown, Minnesota, 156",
+        "registered": "2016-01-15T10:51:54 +05:00"
+    },
+    {
+        "guid": "1358c164-2308-4414-8bc9-acacf0484123",
+        "isActive": true,
+        "name": "Carson Bush",
+        "age": 32,
+        "email": "carsonbush@slambda.com",
+        "phone": "+1 (878) 473-3309",
+        "address": "844 Grand Street, Collins, Florida, 9237",
+        "registered": "2016-03-02T08:31:59 +05:00"
+    },
+    {
+        "guid": "2b6a45d7-7af2-43e4-98ff-479b8d899473",
+        "isActive": true,
+        "name": "White Barrett",
+        "age": 39,
+        "email": "whitebarrett@slambda.com",
+        "phone": "+1 (914) 501-3182",
+        "address": "267 Hastings Street, Enoree, Marshall Islands, 4666",
+        "registered": "2016-10-05T05:06:39 +04:00"
+    },
+    {
+        "guid": "a4182cde-43e6-41d2-8ee2-30e8f6b82850",
+        "isActive": true,
+        "name": "Winnie Adams",
+        "age": 25,
+        "email": "winnieadams@slambda.com",
+        "phone": "+1 (961) 546-2405",
+        "address": "311 Elton Street, Knowlton, American Samoa, 4352",
+        "registered": "2018-01-05T02:41:46 +05:00"
+    },
+    {
+        "guid": "eaa7eaf9-ff83-4e03-89f5-e0b63d35faa5",
+        "isActive": false,
+        "name": "Osborn Mccullough",
+        "age": 26,
+        "email": "osbornmccullough@slambda.com",
+        "phone": "+1 (887) 454-3783",
+        "address": "110 Ryder Avenue, Gratton, Illinois, 114",
+        "registered": "2018-12-27T05:03:45 +05:00"
+    },
+    {
+        "guid": "63245034-6603-44db-af6d-0620c56ba353",
+        "isActive": false,
+        "name": "Karla Lowe",
+        "age": 24,
+        "email": "karlalowe@slambda.com",
+        "phone": "+1 (919) 523-3881",
+        "address": "225 Colin Place, Dixonville, Wisconsin, 7850",
+        "registered": "2016-11-22T05:01:05 +05:00"
+    },
+    {
+        "guid": "4edbc5bc-bf9f-4303-855f-83657d9841d3",
+        "isActive": true,
+        "name": "Kristina Orr",
+        "age": 32,
+        "email": "kristinaorr@slambda.com",
+        "phone": "+1 (941) 471-2023",
+        "address": "390 Williamsburg Street, Lewis, New Hampshire, 4553",
+        "registered": "2018-04-05T02:51:09 +04:00"
+    },
+    {
+        "guid": "e98aa040-305f-4257-b351-cac1f7636dd6",
+        "isActive": true,
+        "name": "Jeannette Keith",
+        "age": 36,
+        "email": "jeannettekeith@slambda.com",
+        "phone": "+1 (917) 599-3804",
+        "address": "339 Erskine Loop, Clarktown, Colorado, 1543",
+        "registered": "2015-06-30T06:06:41 +04:00"
+    },
+    {
+        "guid": "64e9fcf5-1f99-4eeb-9e86-44ed5121f3ac",
+        "isActive": false,
+        "name": "Elba Cardenas",
+        "age": 27,
+        "email": "elbacardenas@slambda.com",
+        "phone": "+1 (808) 600-3028",
+        "address": "364 Murdock Court, Waikele, South Carolina, 1754",
+        "registered": "2019-01-23T08:41:50 +05:00"
+    },
+    {
+        "guid": "e8d459e5-9e41-4b1a-8af2-ace7e3a37829",
+        "isActive": true,
+        "name": "Rhea Flores",
+        "age": 38,
+        "email": "rheaflores@slambda.com",
+        "phone": "+1 (995) 576-3047",
+        "address": "423 Ivan Court, Columbus, Ohio, 6256",
+        "registered": "2015-03-05T05:18:30 +05:00"
+    },
+    {
+        "guid": "89fafae3-0765-4474-a8ff-d4ae07e159c3",
+        "isActive": true,
+        "name": "Cindy Clay",
+        "age": 36,
+        "email": "cindyclay@slambda.com",
+        "phone": "+1 (984) 529-2731",
+        "address": "843 Lawrence Street, Fresno, New Mexico, 3720",
+        "registered": "2018-11-10T06:15:02 +05:00"
+    }
+];
